6c2df02a47ac072595a83778c054cdcee48601c4
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2022-12-31 21:31:14 +0200

    f

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connect = async () => {
-    await mongoose.connect(process.env.DB_connection)
+    await mongoose.connect(process.env.DBURI)
         .then(() => {
             console.log("Database connected");
         }).catch((error) => {
